5b5e0468e27c20134a64c4ef27e74ba8ff73fb24
HybridAir <s.tratton.blake@gmail.com>
2014-05-13 14:32:17 -0700

    Adding in methods from LCDControl.ccp

--- LCDControl.h
+++ LCDControl.h
@@ -6,9 +6,16 @@
 class LCDControl {
     public:
         LCDControl(Options in);
+        printUser();
         void prepareLCD();
+        const int LCDWIDTH = 16;                                                //character width of the LCD
     private:
+        LiquidCrystal lcd(7, 8, 13, 10, 11, 12);                                    //create a new instance of LiquidCrystal, with these pins
         CreateChar(byte code, PGM_P character);
+        clearRow(byte row);
+        int brightness= 0;
+        Color color;
+        const int CONTRASTPIN = 16;                                               //pin used to supply power to the contrast pot
 };
 
 #endif	/* LCDCONTROL_H */
