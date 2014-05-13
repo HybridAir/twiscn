89c01ad9dd275adea0c3637ba3883f30d2f58ebb
HybridAir <s.tratton.blake@gmail.com>
2014-05-13 16:53:57 -0700

    Got all gets set up, and constructor

--- Options.cpp
+++ Options.cpp
@@ -1,3 +1,52 @@
+//handles device options/settings
+#include <Arduino.h>
+
+Options::Options() {                                                            //default constructor, sets up default options
+    brightness = 255;                                                           //LCD brightness
+    color = {0, 150, 255};                                                      //LCD Color
+    blinkColor = {255, 0, 0};                                                   //color the LCD blinks when it gets a new tweet
+    rainbow = false;                                                            //rainbow LCD mode
+    rainSpd = 200;                                                              //rainbow color transition speed
+    blink = false;                                                              //new tweet blink mode
+    blinkSpd = 100;                                                             //tweet blink speed
+}
+
+byte Options::getBrightness() {
+    return brightness;
+}
+
+byte *Options::getCol() {                                                       //returns a pointer to the color array
+    return color;
+}
+
+byte *Options::getBlinkCol() {                                                  //returns a pointer to the blinkColor array
+    return blinkColor;
+}
+
+bool Options::getRainbow() {
+    return rainbow;
+}
+
+int Options::getRainSpd() {
+    return rainSpd;
+}
+
+bool Options::getBlink() {
+    return blink;
+}
+
+byte Options::getBlinkSpd() {
+    return blinkSpd;
+}
+
+
+
+
+
+
+
+
+
 //================================================================
 
 void checkFN1() {
