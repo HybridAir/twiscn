1b3bc74ffc38a45fca0c5afc55f8d50d91c42562
HybridAir <s.tratton.blake@gmail.com>
2014-05-14 15:18:27 -0700

    Removed sleep related code

--- IO.h
+++ IO.h
@@ -3,7 +3,6 @@
 
 #include <Arduino.h>
 #include <avr/wdt.h>                                                            //needed to keep the whole system alive when USB is disconnected
-#include "usbdrv.h"                                                             //the usbSofCount variable requires this
 #include <Bounce.h>
 
 class IO {
@@ -14,7 +13,6 @@ class IO {
         checkConnection();
     private:
         goToSleep();
-        volatile uchar usbSofCount;
         const int CONLED = A4;
         const char FN1PIN = 4;
         const char FN2PIN = A3;
@@ -22,16 +20,10 @@ class IO {
         const char RESETPIN = A1;
         const char LCDPOWPIN = 16;      //pin used to control the contrast pot, and turn contrast on or off
         unsigned long previousMillis = 0;
-        unsigned long previousMillis2 = 0;
-        unsigned long lastSOF = 0;
         const int BLINKTIME = 500;
-        const int SOFDELAY = 500;                                               //max time to wait in between SOF updates
         bool blinkState = false;
         Bounce dbFN1;
         Bounce dbFN2;
-        Comms comms;
-        LCDControl lcd;
-        bool sleeping = false;
 };
 
 #endif	/* IO_H */
