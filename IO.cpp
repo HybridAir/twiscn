f3b23946ac195c59bfb4c3cb9dcc644034768812
HybridAir <s.tratton.blake@gmail.com>
2014-05-14 15:04:07 -0700

    Preparing to move sleep functions from here

--- IO.cpp
+++ IO.cpp
@@ -1,10 +1,10 @@
-//handles basic device IO and sleep
+//handles basic device IO
 #include <Arduino.h>
 #include <avr/wdt.h>                                                            //needed to keep the whole system alive when USB is disconnected
 #include "usbdrv.h"                                                             //the usbSofCount variable requires this
 #include <Bounce.h>
 
-IO::IO(Comms commsin, LCDControl lcdin) {                                       //default constructor, sets up all the inputs and outputs                                                
+IO::IO(LCDControl lcdin) {                                       //default constructor, sets up all the inputs and outputs                                                
     pinMode(RESETPIN, OUTPUT);
     pinMode(CONLED, OUTPUT);
     pinMode(LCDPOWPIN, OUTPUT);
@@ -14,7 +14,7 @@ IO::IO(Comms commsin, LCDControl lcdin) {                                       
     dbFN2 = Bounce(); 
     dbFN1.attach(FN1PIN);
     dbFN2.attach(FN2PIN);
-    comms = commsin;
+    //comms = commsin;
     lcd = lcdin;
 }
 
@@ -42,6 +42,7 @@ void IO::connectionLED(byte mode) {                                             
     }
 }
 
+//make that return a 1 or 2 instead, send it to comms in main
 void IO::checkButtons() {                                                       //checks the debounced buttons for any changes, always run this
     if(dbFN1.update()) {
         if(!dbFN1.read()) {                                                     //if the button was just released
@@ -57,33 +58,33 @@ void IO::checkButtons() {                                                       
 
 //==============================================================================
 
-void IO::checkConnection() {                                                    //check if the device's data connection was disconnected, always run this
-    unsigned long currentMillis = millis();
-    unsigned long currentSOF = usbSofCount;                                     //get the current number of SOFs
-    if(currentMillis - previousMillis2 > SOFDELAY) {                            //only check for new SOF counts every 500 ms (SOFDELAY)
-        previousMillis2 = currentMillis;  
-        if(currentSOF == lastSOF) {                                             //if the SOF counts match (meaning we lost usb connection)  
-            if(!sleeping) {
-                sleeping = true;
-                goToSleep();
-            }
-        }
-        else {                                                                  //the SOF counts are different (still being updated, so it's still connected)
-            lastSOF = currentSOF;
-            sleeping = false;
-        }
-    }
-}
-
-void IO::goToSleep() {
-    lcd.sleepLCD(true);                                                         //turn the LCD off (sleep))
-    connectionLED(0);                                                           //turn the connection LED off, no longer connected
-    while(sleeping) {                                                           //run some checks while the device is "sleeping"
-        usbPoll();
-        sleepDetect();                                                          //check if we still need to be sleeping
-        if (!sleeping) {                                                        //if the previous method said it's time to wake up                                                     
-            lcd.sleepLCD(false);                                                //wake the LCD up                 
-            hostHandshake();                                                    //reconnect to the host
-        }
-    }
-}
+//void IO::checkConnection() {                                                    //check if the device's data connection was disconnected, always run this
+//    unsigned long currentMillis = millis();
+//    unsigned long currentSOF = usbSofCount;                                     //get the current number of SOFs
+//    if(currentMillis - previousMillis2 > SOFDELAY) {                            //only check for new SOF counts every 500 ms (SOFDELAY)
+//        previousMillis2 = currentMillis;  
+//        if(currentSOF == lastSOF) {                                             //if the SOF counts match (meaning we lost usb connection)  
+//            if(!sleeping) {
+//                sleeping = true;
+//                goToSleep();
+//            }
+//        }
+//        else {                                                                  //the SOF counts are different (still being updated, so it's still connected)
+//            lastSOF = currentSOF;
+//            sleeping = false;
+//        }
+//    }
+//}
+//
+//void IO::goToSleep() {
+//    lcd.sleepLCD(true);                                                         //turn the LCD off (sleep))
+//    connectionLED(0);                                                           //turn the connection LED off, no longer connected
+//    while(sleeping) {                                                           //run some checks while the device is "sleeping"
+//        usbPoll();
+//        sleepDetect();                                                          //check if we still need to be sleeping
+//        if (!sleeping) {                                                        //if the previous method said it's time to wake up                                                     
+//            lcd.sleepLCD(false);                                                //wake the LCD up                 
+//            hostHandshake();                                                    //reconnect to the host
+//        }
+//    }
+//}
