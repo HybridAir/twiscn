4b87b40efefdf74d787adcffcd6d2130fabf6b0e
HybridAir <s.tratton.blake@gmail.com>
2014-05-14 16:46:36 -0700

    Got input checking done

--- main.cpp
+++ main.cpp
@@ -1,96 +1,61 @@
 #include <Arduino.h>
-//#include <Bounce.h>          //used for debouncing the FN buttons
-//#include <LiquidCrystal.h>
-//#include <PString.h>
-#include <avr/pgmspace.h>
-#include "usbdrv.h"                                                             //the usbSofCount variable requires this
+#include "usbdrv.h"
+#include "IO.h"
 #include <avr/wdt.h>                                                            //needed to keep the whole system alive when USB is disconnected
 
-setup();
-loop();
-checkConnection();
-goToSleep();
+void setup();
+void loop();
+void checkConnection();
+void goToSleep();
+void doButtons();
+void doSpeedPot();
+void doSleep();
 
-const int RESETPIN = A1;
 volatile uchar usbSofCount;
 bool sleeping = false;
 const int SOFDELAY = 500;                                               //max time to wait in between SOF updates
 unsigned long lastSOF = 0;
 unsigned long previousMillis2 = 0;
-
     
+IO inout;
+Options opt;
+TweetHandler twt;
+LCDControl lcd;
+Comms comms;
 
+//==============================================================================
 
-
-void setup() {
-    digitalWrite(RESETPIN, HIGH);                                               //this needs to be set high before anything so the device doesn't get reset 
+void setup() {  
+    inout = IO inout();                                                         //create the instance and set up all inputs and outputs
+    opt = Options opt();                                                        //create the instance and set up default options
+    twt = TweetHandler twt();                                                   //new instance of TweetHandler, doesn't require anything
+    lcd = LCDControl lcd(opt, twt);                                             //new instance, set up LCD and play the boot animation
+    comms = Comms comms(opt, inout, twt);                                       //new instance, set up usb comms
     
-    IO io();                                                                    //create the instance and set up all inputs and outputs
-    Options opt();                                                              //create the instance and set up default options
-    TweetHandler twt();                                                         //new instance of TweetHandler, doesn't require anything
-    LCDControl lcd(opt, twt);                                                   //new instance, set up LCD and play the boot animation
-    Comms comms(opt, io, twt);                                                  //new instance, set up usb comms
-  
     comms.handshake();                                                          //establish a connection with the host program
 }
 
-//========================================================
-
-void loop()
-{
-    
-    
-    
-    
-    
-//  usbPoll();
-//  //sleepWait();
-//  
-//  //if (readyToSleep == 1) {
-//    sleepDetect();
-//    sleepState();
-//  //}
-//  
-//  if (sleepStatus == 0) {
-//
-//    newTweetBlink();
-//    checkRainbow();
-//    checkFN1();
-//    checkFN2();
-//    checkpot(); 
-//  
-//    checkcomms();
-//    checkmode(); 
-//    
-//    checkfrozen();   //see if the lcd is supposed to be frozen, and if it needs to unfreeze
-//
-//    scrolltext();   
-//    if (firstTweet == 0) {
-//      if (waitforbegin == 0) {
-//        if (greaterThan16 == 1) {
-//          printtext();   //actually print the text
-//        }
-//      } 
-//    }  
-//  }
+void loop() {
+    comms.readComms();
+    doButtons();
+    doSpeedPot();
+    doSleep();
 }
 
+//==============================================================================
 
-
-
-//=======================================================================
-
-void checkpot() {
-  finalSpeed = analogRead(SPEEDPIN) / 2;   //get the value of the speed potentiometer, divided by 2
-  //finalspeed = potvalue / 2;   //compute that value
+void doButtons() {
+    byte btn = inout.checkButtons();
+    if(btn != 0) {
+        comms.sendBtn(btn);
+    }
 }
 
+void doSpeedPot() {
+    lcd.setSpeed(inout.checkPot());
+}
 
-
-
-//keep sleep stuff out here actually
-
-void IO::checkConnection() {                                                    //check if the device's data connection was disconnected, always run this
+void checkConnection() {                                                    //check if the device's data connection was disconnected, always run this
     unsigned long currentMillis = millis();
     unsigned long currentSOF = usbSofCount;                                     //get the current number of SOFs
     if(currentMillis - previousMillis2 > SOFDELAY) {                            //only check for new SOF counts every 500 ms (SOFDELAY)
@@ -120,5 +85,3 @@ void IO::goToSleep() {
         }
     }
 }
-
-
