b9441da7362b7e77485d0e01252bf3136de16ac5
HybridAir <s.tratton.blake@gmail.com>
2014-05-14 14:32:38 -0700

    Set up empty methods

--- TweetHandler.cpp
+++ TweetHandler.cpp
@@ -1,1 +1,41 @@
 //used for holding and handling tweets
+
+#include <Arduino.h>
+
+TweetHandler::TweetHandler() { 
+
+}
+
+void TweetHandler::setUser(String in) {
+    
+}
+
+void TweetHandler::setTweet(String in) {
+    
+}
+
+void TweetHandler::setPrevUser(String in) {
+    
+}
+
+void TweetHandler::setPrevTweet(String in) {
+    
+}
+
+//==============================================================================
+
+String TweetHandler::getUser() {
+    
+}
+
+String TweetHandler::getTweet() {
+    
+}
+
+String TweetHandler::getPrevUser() {
+    
+}
+
+String TweetHandler::getPrevTweet() {
+    
+}
