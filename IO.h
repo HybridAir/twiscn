ed574ab502ea1d92bec149e4581925893d78de53
HybridAir <s.tratton.blake@gmail.com>
2014-05-14 14:02:02 -0700

    Added additional sleep related variables and methods

--- IO.h
+++ IO.h
@@ -9,8 +9,29 @@
 class IO {
     public:
         IO();
+        connectionLED(int in);
+        checkButtons();
+        checkConnection();
     private:
+        goToSleep();
         volatile uchar usbSofCount;
+        const int CONLED = A4;
+        const char FN1PIN = 4;
+        const char FN2PIN = A3;
+        const char SPEEDPIN = A0;
+        const char RESETPIN = A1;
+        const char LCDPOWPIN = 16;      //pin used to control the contrast pot, and turn contrast on or off
+        unsigned long previousMillis = 0;
+        unsigned long previousMillis2 = 0;
+        unsigned long lastSOF = 0;
+        const int BLINKTIME = 500;
+        const int SOFDELAY = 500;                                               //max time to wait in between SOF updates
+        bool blinkState = false;
+        Bounce dbFN1;
+        Bounce dbFN2;
+        Comms comms;
+        LCDControl lcd;
+        bool sleeping = false;
 };
 
 #endif	/* IO_H */
