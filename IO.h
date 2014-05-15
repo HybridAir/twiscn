d70bb7f3dca71913647b85b7fc0ee4833c856b8c
HybridAir <s.tratton.blake@gmail.com>
2014-05-14 22:28:14 -0700

    Updated to reflect changes in the cpp

--- IO.h
+++ IO.h
@@ -10,16 +10,37 @@ class IO {
         connectionLED(byte mode);
         checkButtons();
         checkPot();
+        setBacklight(uint8_t r, uint8_t g, uint8_t b, byte brightness);
+        enableTweetBlink(bool in);
+        tweetBlink();
+        setBlinkSpeed(byte in);
+        rainbow();
     private:
-        const int CONLED = A4;
+        const int CONLED = A4;                                                  //connection led pin
         const char FN1PIN = 4;
         const char FN2PIN = A3;
-        const char SPEEDPIN = A0;
-        const char RESETPIN = A1;
-        const char LCDPOWPIN = 16;      //pin used to control the contrast pot, and turn contrast on or off
+        const char SPEEDPIN = A0;                                               //pin the speed pot is
+        const char RESETPIN = A1;                                               //never needed to be implemented, but we're stuck with it now
+        const char LCDPOWPIN = 16;                                              //pin used to control power to the contrast pot, turns contrast on/off
+        const char REDLITE = 9;
+        const char GREENLITE = 5;
+        const char BLUELITE = 6;
         unsigned long previousMillis = 0;
-        const int BLINKTIME = 500;
-        bool blinkState = false;
+        unsigned long previousMillis5 = 0;
+        unsigned long previousMillis6 = 0;
+        const int BLINKTIME = 500;                                              //time between connection led state changes
+        bool blinkState = false;                                                //controls whether the connection led needs to change states
+        bool blinkEnabled = false;
+        byte blinkSpeed;                                                        //time between tweet blinks
+        int rainSpeed;                                                          //time between rainbow increments
+        byte currentColor = 0;                                                  //current color section that is being faded though
+        byte rainLevel = 0;
+        byte red;
+        byte green;
+        byte blue;
+        byte bRed;
+        byte bGreen;
+        byte bBlue;
         Bounce dbFN1;
         Bounce dbFN2;
 };
