8a1c34267999be911657d8105a2e48a960fafc93
HybridAir <s.tratton.blake@gmail.com>
2014-05-15 11:56:19 -0700

    Updated to reflect changes in the cpp

--- TweetHandler.h
+++ TweetHandler.h
@@ -5,8 +5,20 @@
 
 class TweetHandler {
     public:
-        TweetHandler();
+        TweetHandler(LCDControl lcdin);
+        setUser(String in);
+        setTweet(String in);
+        getUser();
+        getTweet();
+        getPrevUser();
+        getPrevTweet();
+        sendTweet();
     private:
+        LCDControl lcd;
+        String user;
+        String tweet;
+        String prevUser;
+        String prevTweet;
 };
 
 #endif	/* TWEETHANDLER_H */
