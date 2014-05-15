e017f7aaff57fb855376c6c4c414ce923b246dd2
HybridAir <s.tratton.blake@gmail.com>
2014-05-15 11:56:02 -0700

    Added tweet sending code, and filled in empty methods

--- TweetHandler.cpp
+++ TweetHandler.cpp
@@ -2,40 +2,46 @@
 
 #include <Arduino.h>
 
-TweetHandler::TweetHandler() { 
-
+TweetHandler::TweetHandler(LCDControl lcdin) { 
+    lcd = lcdin;
 }
 
 void TweetHandler::setUser(String in) {
-    
+    prevUser = user;                                                            //save a copy of the current (now previous) user
+    user = in;                                                                  //set the new user
 }
 
 void TweetHandler::setTweet(String in) {
-    
-}
-
-void TweetHandler::setPrevUser(String in) {
-    
-}
-
-void TweetHandler::setPrevTweet(String in) {
-    
+    prevTweet = tweet;                                                          //save a copy of the current (now previous) tweet
+    tweet = in;                                                                 //set the new tweet
 }
 
 //==============================================================================
 
 String TweetHandler::getUser() {
-    
+    return user;
 }
 
 String TweetHandler::getTweet() {
-    
+    return tweet;
 }
 
 String TweetHandler::getPrevUser() {
-    
+    return prevUser;
 }
 
 String TweetHandler::getPrevTweet() {
-    
+    return prevTweet;
+}
+
+//==============================================================================
+
+String TweetHandler::sendTweet() {
+    if(tweet.length() =< lcd.LCDWIDTH) {
+        lcd.printNewTweet(user, tweet);
+    }
+    else {
+        String beginning = tweet.substring(0, 15);
+        lcd.printNewTweet(lcd, beginning, tweet); 
+    }
 }
