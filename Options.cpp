4bb1871d4b4cfff8d6d554963466f39c3d23b502
HybridAir <s.tratton.blake@gmail.com>
2014-05-14 22:27:34 -0700

    Removed backlight code

--- Options.cpp
+++ Options.cpp
@@ -1,7 +1,10 @@
 //handles device options/settings
 #include <Arduino.h>
 
-Options::Options() {                                                            //default constructor, sets up default options
+#include "IO.h"
+
+Options::Options(IO ioin) {                                                     //default constructor, sets up default options
+    inout = ioin;
     brightness = 255;                                                           //LCD brightness
     color = {0, 150, 255};                                                      //LCD Color
     blinkColor = {255, 0, 0};                                                   //color the LCD blinks when it gets a new tweet
@@ -45,10 +48,12 @@ byte Options::getBlinkSpd() {
 
 void Options::setBrightness(byte in) {
     brightness = in;
+    inout.setBacklight(color[0], color[1], color[2], brightness);
 }
 
 void Options::setCol(byte r, byte g, byte b) {
     color = {r, g, b};
+    inout.setBacklight(color[0], color[1], color[2], brightness);
 }
 
 void Options::setBlinkCol(byte r, byte g, byte b) {                                  
@@ -65,10 +70,20 @@ void Options::setRainSpd(int in) {
 
 void Options::setBlink(bool in) {
     blink = in;
+    //inout.enableTweetBlink(blink);
 }
 
 void Options::setBlinkSpd(byte in) {
     blinkSpd = in;
+    inout.setBlinkSpeed(blinkSpd);
+}
+
+void Options::updateCol() {                                                     //force update the backlight color
+    inout.setBacklight(color[0], color[1], color[2], brightness);
+}
+
+void Options::updateBlinkCol() {                                                //force update the tweet blink backlight color
+    inout.setBacklight(blinkColor[0], blinkColor[1], blinkColor[2], brightness);
 }
 
 //==============================================================================
@@ -136,102 +151,3 @@ void Options::getRainbow(String in) {                                           
     String spd = in.charAt(1, 3);                                               //get a substring containing the blink speed value out
     setRainSpd(spd.toInt());                                                    //set the speed to that value converted to an int
 }
-
-
-
-
-
-
-void setBacklight(uint8_t r, uint8_t g, uint8_t b) {
-    r = map(r, 0, 255, 0, brightness);
-    g = map(g, 0, 255, 0, brightness);
-    b = map(b, 0, 255, 0, brightness);
-
-    // common anode so invert!
-    r = map(r, 0, 255, 255, 0);
-    g = map(g, 0, 255, 255, 0);
-    b = map(b, 0, 255, 255, 0);
-
-    analogWrite(REDLITE, r);
-    analogWrite(GREENLITE, g);
-    analogWrite(BLUELITE, b);
-}
-
-void newTweetBlink() {
-    if (tweetBlinkEnabled == 1) {
-        if (timeToBlink == 1) {
-            currentMillis5 = millis();
-            if(currentMillis5 - previousMillis5 > blinkTime) {
-                previousMillis5 = currentMillis5;
-                if (blinkCount <= 4) {
-                    if (blinkStatus == 0) {
-                        brightness = 255;
-                        setBacklight(r, g, b);
-                        blinkStatus = 1;
-                    }
-                    else {
-                        brightness = 255;
-                        setBacklight(r2, g2, b2);
-                        blinkStatus = 0;
-                    }
-                    if (blinkCount == 4) {
-                        blinkCount = 0;
-                        timeToBlink = 0;
-                    }
-                    else {
-                        blinkCount++;
-                    }
-                }
-            }
-        }
-        else if (timeToBlink == 0) {
-            brightness = 255;
-            setBacklight(r, g, b);      
-        }
-    }
-}
-
-void checkRainbow() {
-    if (rainbow == 1) {   //do this if rainbow mode is on
-        howLongItsBeen = millis() - lastTimeItHappened;
-            if ( howLongItsBeen >= howLongToWait ) {   //if it's time, ycle through by one increment
-      
-        //Cycle through the rainbow
-                if (color1 == 1) {   //do this if we're at the first rainbow section
-                    if (rain < 255) {   //and only if we're below 255 increments already
-                        setBacklight(rain, 0, 255-rain);
-                        rain++;
-                    }
-                    else {   //if we're not in the first section, go to the next
-                        color1 = 0;
-                        color2 = 1;
-                        rain = 0;   //reset the increment amount
-                    }
-                } 
-                if (color2 == 1) {
-                    if (rain < 255) {
-                        setBacklight(255-rain, rain, 0);
-                        rain++;
-                    }
-                    else {
-                        color2 = 0;
-                        color3 = 1;
-                        rain = 0;
-                    }
-                } 
-                if (color3 == 1) {
-                    if (rain < 255) {
-                        setBacklight(0, 255-rain, rain);
-                        rain++;
-                    }
-                    else {
-                        color3 = 0;
-                        color1 = 1;
-                        rain = 0;
-                    }
-                }  
-   
-                lastTimeItHappened = millis();   //save the last time we cycled through the rainbow by one
-            }
-        }
-    }
