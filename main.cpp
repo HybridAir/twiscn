a6fca456a3903015ede801c8931146ed5b68310a
HybridAir <s.tratton.blake@gmail.com>
2014-05-17 15:26:18 -0700

    fixed formatting, added/change comments

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
-#include <Arduino.h>
+//TwitterScreen Device code
+//TODO: demo mode maybe, previous tweets
+#include <Arduino.h>                                                            //used for its nice methods and stuff
 #include "usbdrv.h"                                                             //needed for SOF counts
 #include <avr/wdt.h>                                                            //needed to keep the whole system alive when USB is disconnected
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h>                                                      //used to operate the LCD
+
+//included class headers:
 #include "Comms.h"
 #include "IO.h"
 #include "LCDControl.h"
 #include "Options.h"
 #include "TweetHandler.h"
-//#include "classes.h"
 
-//maybe add a demo mode?
-
+//prototype declaration
 void setup();
 void loop();
 void checkConnection();
@@ -19,60 +21,36 @@ void doButtons();
 void checkForSleep();
 void prepare();
 
+//global variables, shouldn't hurt anything
 volatile uchar usbSofCount;                                                     //holds the current SOF count
-bool sleeping = false;
-const unsigned int SOFDELAY = 500;                                                       //max time to wait in between SOF checks
-unsigned long lastSOF = 0;
-unsigned long previousMillis2 = 0;
+bool sleeping = false;                                                          //stores the sleep status
+const unsigned int SOFDELAY = 500;                                              //max time to wait in between SOF checks
+unsigned long lastSOF = 0;                                                      //last time an SOF happened in ms
+unsigned long previousMillis2 = 0;                                              //used for keeping track of SOF checking times
 const int LCDWIDTH = 16;                                                        //character width of the LCD
     
-//Options opt;                                                        //create the instance and set up default options
-//IO inout;                                                      //create the instance and set up all inputs and outputs
-//TweetHandler twt;                                           //create the instance for use by other classes
-//LCDControl lcd;                                   //new instance, set up LCD and play the boot animation
-//Comms comms;                                  //new instance, set up usb comms
-
-LiquidCrystal lcdc(7, 8, 13, 10, 11, 12);
-
-Options opt;
-IO inout;
-TweetHandler twt(LCDWIDTH);
-LCDControl lcd(LCDWIDTH);
-Comms comms;
-
-
+//global class initialization
+LiquidCrystal lcdc(7, 8, 13, 10, 11, 12);                                       //new instance of the LiquidCrystal class, give it the pins
+Options opt;                                                                    //new instance of options
+IO inout;                                                                       //new instance of io
+TweetHandler twt(LCDWIDTH);                                                     //new instance of TweetHandler, needs the LCDWIDTH
+LCDControl lcd(LCDWIDTH);                                                       //new instance of LCDControl, needs the LCDWIDTH
+Comms comms;                                                                    //new instance of comms
 
 //==============================================================================
 
 void setup() {  
-//    opt = Options();                                                        //create the instance and set up default options
-//    inout = IO(opt);                                                      //create the instance and set up all inputs and outputs
-//    twt = TweetHandler(LCDWIDTH);                                           //create the instance for use by other classes
-//    lcd = LCDControl(opt, twt, LCDWIDTH);                                   //new instance, set up LCD and play the boot animation
-//    comms = Comms(opt, inout, twt, lcd);                                  //new instance, set up usb comms
-    
-//    opt = new Options();                                                        //create the instance and set up default options
-//    inout = new IO(opt);                                                      //create the instance and set up all inputs and outputs
-//    twt = new TweetHandler(LCDWIDTH);                                           //create the instance for use by other classes
-//    lcd = new LCDControl(opt, twt, LCDWIDTH);                                   //new instance, set up LCD and play the boot animation
-//    comms = new Comms(opt, inout, twt, lcd);                                  //new instance, set up usb comms
-    
-//    opt.begin();
-//    inout.begin(opt);
-//    lcd.begin(opt, twt);
-//    comms.begin(opt, inout, twt, lcd);
-    
     prepare();                                                                  //prepare the device for operation
 }
 
 void loop() {
-    comms.readComms();
-    doButtons();
-    lcd.setSpeed(inout.checkPot());
-    inout.rainbow();
-    lcd.scrollTweet();
-    inout.tweetBlink();
-    checkForSleep();
+    comms.readComms();                                                          //checks for any new comms data and processes it
+    doButtons();                                                                //monitors button changes and processes them
+    lcd.setSpeed(inout.checkPot());                                             //applies any changes made to the speed pot
+    inout.rainbow();                                                            //control the rainbow backlight changes                                                        
+    lcd.scrollTweet();                                                          //scrolls the tweet
+    inout.tweetBlink();                                                         //blinks the lcd if any new tweets are displayed
+    checkForSleep();                                                            //checks if the device needs to be sleeping
 }
 
 //==============================================================================
@@ -83,9 +61,9 @@ void prepare() {                                                                
 }
 
 void doButtons() {                                                              //used to check the function buttons, must be continuously ran
-    byte btn = inout.checkButtons();
-    if(btn != 0) {
-        comms.sendBtn(btn);
+    byte btn = inout.checkButtons();                                            //check for any button changes, get the button that was changed out
+    if(btn != 0) {                                                              //if there was a change
+        comms.sendBtn(btn);                                                     //send the button that changed to the host program
     }
 }
 
