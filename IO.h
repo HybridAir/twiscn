aaacd8e26a65a26f1606ab1d107633c453904fea
HybridAir <s.tratton.blake@gmail.com>
2014-05-18 20:45:52 -0700

    Removed unnecessary code and organized

--- IO.h
+++ IO.h
@@ -2,43 +2,38 @@
 #define	IO_H
 
 #include <Arduino.h>
-#include <Bounce.h>
+#include <Bounce.h>                                                             //required for debouncing
 #include "Options.h"
 #include "LCDControl.h"
 
-
+#define CONLED A4                                                               //connection led pin
+#define FN1PIN 4                                                                //FN1 button
+#define FN2PIN A3                                                               //FN2 button
+#define SPEEDPIN A0                                                             //pin the scroll speed pot is on
+#define LCDPOWPIN 16                                                            //pin used to control power to the contrast pot, no longer needs to be used (just keep HIGH)
+//#define RESETPIN A1                                                           //never needed to be implemented, but might as well keep it in here
+//pins for each of the lcd's backlight leds
+#define REDLITE 9
+#define GREENLITE 5
+#define BLUELITE 6
 
 class IO {
     public:
         IO();
-        void connectionLED(byte mode);
         byte checkButtons();
         int checkPot();
+        void connectionLED(byte mode);
         void setBacklight(uint8_t r, uint8_t g, uint8_t b, byte brightness);
-        //enableTweetBlink(bool in);
         void tweetBlink();
-        //void setBlinkSpeed(byte in);
         void rainbow();
-        void testLed();
     private:
-        //Options opt;
-         int CONLED;                                                  //connection led pin
-         char FN1PIN;
-         char FN2PIN;
-         char SPEEDPIN;                                               //pin the speed pot is
-         char RESETPIN;                                               //never needed to be implemented, but we're stuck with it now
-         char LCDPOWPIN;                                              //pin used to control power to the contrast pot, turns contrast on/off
-         char REDLITE;
-         char GREENLITE;
-         char BLUELITE;
         unsigned long previousMillis;
         unsigned long previousMillis5;
         unsigned long previousMillis6;
-         int BLINKTIME;                                              //time between connection led state changes
+        int blinkTime;                                              //time between connection led state changes
         bool blinkState;                                                //controls whether the connection led needs to change states
         bool blinkEnabled;
         byte blinkSpeed;                                                        //time between tweet blinks
-        int rainSpeed;                                                          //time between rainbow increments
         byte currentColor;                                                  //current color section that is being faded though
         byte rainLevel;
         byte red;
@@ -47,14 +42,10 @@ class IO {
         byte bRed;
         byte bGreen;
         byte bBlue;
-        byte rain;
         Bounce dbFN1;
         Bounce dbFN2;
-        byte blinkCount;
-                bool blinking;
+        byte blinkCount;               
 };
-
-//extern IO inout;
 
 #endif	/* IO_H */
 
