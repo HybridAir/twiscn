8ca3a7b04acc8374fd3d4515c9ab42f95624b051
HybridAir <s.tratton.blake@gmail.com>
2014-05-19 12:13:22 -0700

    Organized and updated comments

--- Comms.cpp
+++ Comms.cpp
@@ -5,31 +5,27 @@ extern Options opt;
 extern IO inout;
 extern TweetHandler twt;
 extern LCDControl lcd;
+extern LiquidCrystal lcdc;
 
-Comms::Comms() {    //onstructor
-    
-//    opt = optin;
-//    io = ioin;
-//    twt = twtin;
-//    lcd = lcdin;
-    usb.begin(); 
+Comms::Comms() {                                                                //default constructor
+    usb.begin();                                                                //start up the usb hidserial connection
     gotUser = false;
     gotTweet = false;
-    connected = false;//start the hidserial connection
+    connected = false;                                                          //considering that this was just started, we will not be connected yet
 }
 
-void Comms::readComms() {                                                       //checks if we got anything new from the host, and then processes it
-    usbPoll();
+void Comms::readComms() {                                                       //checks if we got anything new from the host, and then processes it, run this continuously
+    usbPoll();                                                                  //make sure to run this as often as possible
     if (usb.available()) {                                                      //check if there's something in the usb buffer
-        usb.read((uint8_t*)usbBuffer);                                          //put the data into a string (probably dont need the uint8_t*)
-        char inByte = (uint8_t)usbBuffer[0];                                      //first character is used to identify the data packet type
+        usb.read((uint8_t*)usbBuffer);                                          //put the data into a string
+        char inByte = (uint8_t)usbBuffer[0];                                    //first character is used to identify the data packet type
         usbBufStr = String(usbBuffer);
     
         switch (inByte) {                                                       //check what character it is, and process accordingly
             case '=':                                                           //marks the end of the entire transfer, must always be in its own packet     
                 checkType();                                                    //process the completed data transfer
                 break;     
-            default:                                                            //this will only trigger if we got transfer type signifier (!, @, etc)            
+            default:                                                            //this will only trigger for regular packet transfers           
                 transferOut += usbBufStr;                                       //add the current packet to the output transfer String
                 break;
         }
@@ -42,43 +38,49 @@ void Comms::checkType() {                                                       
     switch(type) {                                                              //check the first char of the transferOut String
         case '@':                                                               //username transfer, also signifies the start of a new tweet
             userOut = transferOut;                                              //put the transfer into a new String for the username
-            transferOut = "";
-            gotUser = true;                               
+            transferOut = "";                                                   //empty transferOut so it can accept a new transfer
+            gotUser = true;                                                     //we got the user               
             break;
-        case '!':                                                               //tweet transfer, ending for a new tweet     
+        case '!':                                                               //tweet transfer, ending of a new tweet     
             twtOut = transferOut;                                               //put the transfer into a new String for the tweet
-            transferOut = "";                                                   //empty the transferOut to make room for a new one
-            gotTweet = true;
+            transferOut = "";                                                   //empty transferOut so it can accept a new transfer
+            gotTweet = true;                                                    //we got the tweet text
             break;
         case '$':                                                               //option transfer
-            opt.extractOption(transferOut);                                     //get the option data out of the transfer
-            transferOut = "";                                                   //empty the transferOut to make room for a new one
+            String out = transferOut;                                           //get the transferOut into a new string
+            transferOut = "";                                                   //empty transferOut so it can accept a new transfer
+            opt.extractOption(out);                                             //get the option data out of the transfer
             break;   
     }
     if (gotUser & gotTweet) {                                                   //if we got both the tweet and the user
-        twt.setUser(userOut);                                                    //give the tweet handler a new user
-        twt.setTweet(twtOut);                                                  //give the tweet handler a new tweet
+        twt.setUser(userOut);                                                   //give the tweet handler a new user
+        twt.setTweet(twtOut);                                                   //give the tweet handler a new tweet
         lcd.printNewTweet();                                                    //tell LCDControl to print the new tweet
-        gotTweet = false;                                                       //already got the new tweet, so reset those vars
+        //already got the new tweet, so reset those vars
+        gotTweet = false;                                                       
         gotUser = false;
     }   
 }
 
 void Comms::handshake() {                                                       //used to establish a data connection with the host
     while (!connected) {                                                        //do this while we are not connected
-        lcd.connectDisplay(true);                                                  //display the connecting animation on the LCD
-        inout.connectionLED(2);                                                    //blink the connection led to further signify that the device is connecting
+        lcd.connectDisplay(true);                                               //display the connecting animation on the LCD
+        inout.connectionLED(2);                                                 //blink the connection led to further signify that the device is connecting
         usbPoll();                                                              //keep polling the USB port for any new data
         usb.println("`");                                                       //continuously send this to the host so it knows the we are waiting for a handshake
         if (usb.available()) {                                                  //check if we got any data from the host
             usb.read((uint8_t*)usbBuffer);                                      //read that data into usbBuffer
-            if ((uint8_t)usbBuffer[0] == '~') {                                   //a "~" is the host acknowledging that it got the "`" from before
+            if ((uint8_t)usbBuffer[0] == '~') {                                 //a "~" is the host acknowledging that it got the "`" from before
                 connected = true;                                               //and now we are connected
             }
         }  
     }
-    inout.connectionLED(1);                                                        //turn the connection led solid on since we're connected now
-    lcd.connectDisplay(false);                                                     //show the connected notice on the lcd
+    inout.connectionLED(1);                                                     //turn the connection led solid on since we're connected now
+    lcd.connectDisplay(false);                                                  //show the connected notice on the lcd
+}
+
+void Comms::setConnected(bool in) {                                             //sets the connected status
+    connected = in;
 }
 
 void Comms::sendBtn(byte in) {                                                  //used to send button presses to the host program for processing
