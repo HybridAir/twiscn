84eddafa24ae2c144af39e93baf17502ed6438b6
HybridAir <s.tratton.blake@gmail.com:>
2014-05-18 17:20:49 -0700

    another test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 //TwitterScreen Device code
-//TODO: demo mode maybe, previous tweets
+//TODO: demo mode maybe, previous tweets, change rainbow mode time to int (in comms), detection for host handshake while running
 #include <Arduino.h>                                                            //used for its nice methods and stuff
 #include "usbdrv.h"                                                             //needed for SOF counts
 #include <avr/wdt.h>                                                            //needed to keep the whole system alive when USB is disconnected
