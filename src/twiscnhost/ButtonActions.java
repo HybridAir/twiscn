fe6daaadf26b45cdb8548711ddb04de3d659c414
HybridAir <s.tratton.blake@gmail.com>
2014-05-20 10:17:10 -0700

    Renamed

---
+++ src/twiscnhost/ButtonActions.java
@@ -0,0 +1,52 @@
+//used to do things to the device when it sends button presses to us; fn button actions
+
+package twiscnhost;
+
+public class ButtonActions {
+    //brightness switching (on/off, levels)
+    //rainbow mode toggle
+    //previous/current tweet
+    //scroll pause/resume
+    //preset color switching
+    
+    
+    
+    //    public void fnHandler(int in) {
+//        if(in == 1) {
+//            switchBrightness();
+//        }
+//        else if(in == 2) {
+//            
+//        }
+//    }
+//    
+//    private void switchBrightness() {
+//        
+//        b++;
+//        if(b == 6) {
+//            b = 0;
+//        }
+//        switch(b) {
+//            case 0:
+//                brightness = 255;
+//                break;
+//            case 1:
+//                brightness = 128;
+//                break;
+//            case 2:
+//                brightness = 64;
+//                break;
+//            case 3:
+//                brightness = 32;
+//                break;
+//            case 4:
+//                brightness = 16;
+//                break;
+//            case 5:
+//                brightness = 0;
+//                break;
+//            default:
+//                break;
+//            }
+//    }
+}
