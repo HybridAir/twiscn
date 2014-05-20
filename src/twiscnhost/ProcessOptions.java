1de7e1a5460d5c744ce122e5b53268b67c093423
HybridAir <s.tratton.blake@gmail.com>
2014-05-20 09:56:44 -0700

    Updated comments in preparation for implementation

--- src/twiscnhost/ProcessOptions.java
+++ src/twiscnhost/ProcessOptions.java
@@ -1,4 +1,4 @@
-//used to do things to the device when it sends button presses to us
+//used to do things to the device when it sends button presses to us; fn button actions
 
 package twiscnhost;
 
@@ -6,5 +6,47 @@ public class ProcessOptions {
     //brightness switching (on/off, levels)
     //rainbow mode toggle
     //previous/current tweet
+    //scroll pause/resume
     //preset color switching
+    
+    
+    
+    //    public void fnHandler(int in) {
+//        if(in == 1) {
+//            switchBrightness();
+//        }
+//        else if(in == 2) {
+//            
+//        }
+//    }
+//    
+//    private void switchBrightness() {
+//        
+//        b++;
+//        if(b == 6) {
+//            b = 0;
+//        }
+//        switch(b) {
+//            case 0:
+//                brightness = 255;
+//                break;
+//            case 1:
+//                brightness = 128;
+//                break;
+//            case 2:
+//                brightness = 64;
+//                break;
+//            case 3:
+//                brightness = 32;
+//                break;
+//            case 4:
+//                brightness = 16;
+//                break;
+//            case 5:
+//                brightness = 0;
+//                break;
+//            default:
+//                break;
+//            }
+//    }
 }
