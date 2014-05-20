ea3d3f21ef38b4dda99a65e933069866735c020b
HybridAir <s.tratton.blake@gmail.com>
2014-05-19 17:47:05 -0700

    Initial creation

---
+++ src/twiscnhost/ProcessOptions.java
@@ -0,0 +1,10 @@
+//used to do things to the device when it sends button presses to us
+
+package twiscnhost;
+
+public class ProcessOptions {
+    //brightness switching (on/off, levels)
+    //rainbow mode toggle
+    //previous/current tweet
+    //preset color switching
+}
