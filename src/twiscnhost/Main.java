f5b43ec9de01d18b51b8245083bc86034f6da19a
HybridAir <s.tratton.blake@gmail.com>
2014-05-20 10:06:37 -0700

    Updated comments

--- src/twiscnhost/Main.java
+++ src/twiscnhost/Main.java
@@ -1,5 +1,5 @@
 //Main console-based host program for controlling the TwiScnDevice
-//TODO: options saving/loading, twitter implementation, gui (tray application maybe?) 
+//TODO: detect handshake while running?, options saving/loading (use Properties), twitter implementation, gui (tray application) 
 package twiscnhost;
 
 public class Main {
@@ -7,14 +7,14 @@ public class Main {
     public static final int VENDOR_ID = 0x16c0;                         
     public static final int PRODUCT_ID = 0x27d9;
     public static final int[] DEVICEIDS = {VENDOR_ID, PRODUCT_ID};
-    public static DeviceOptions devOptions;
+    public static Options devOptions;
 
     public static void main(String[] args) {
-        devOptions = new DeviceOptions();                                       //create a new instance of DeviceOptions
+        devOptions = new Options();                                             //create a new instance of DeviceOptions
         DeviceHandler twiScn = new DeviceHandler(DEVICEIDS, devOptions);        //create a new instance of DeviceHandler, needs the device ids and options      
                
         //set up and send a dummy tweet
-        String tweet = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma";
+        String tweet = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque";
         String user = "Blake:";
         twiScn.newTweet(user, tweet);
         
