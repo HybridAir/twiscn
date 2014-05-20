7c4ccaf72498ba2c8aaae850543c60b12f89f193
HybridAir <s.tratton.blake@gmail.com>
2014-05-20 11:15:17 -0700

    Added brightness switching code, and general supporting code

--- src/twiscnhost/ButtonActions.java
+++ src/twiscnhost/ButtonActions.java
@@ -3,50 +3,84 @@
 package twiscnhost;
 
 public class ButtonActions {
-    //brightness switching (on/off, levels)
-    //rainbow mode toggle
-    //previous/current tweet
-    //scroll pause/resume
-    //preset color switching
+    
+    private Options opt;
+    private int brightness;
+    private byte brightState = 0;
+    
+    public ButtonActions(Options opt) {
+        this.opt = opt;
+        brightness = opt.getBrightnessInt();
+    }
+    
+    public void fn1() {                                                         //call this whenever we get a response from FN1 (make sure to applyOptions after)
+        doAction(opt.getFn1Action());
+    }
+    
+    private void doAction(byte type) {                                          //used to call different functions depending on the byte passed in
+        switch(type) {
+            case 0:
+                switchBrightness();
+                break;
+            case 1:
+                toggleRainbow();
+                break;
+            case 2:
+                prevTweet();
+                break;
+            case 3:
+                pauseScroll();
+                break;
+            case 4:
+                switchColor();
+                break;
+            default:
+                break;
+                
+                            
+        }
+    }
+    
+    private void switchBrightness() {                                           //used to switch through backlight brightness levels exponentially and on/off
+        switch(brightState) {                                                   //switch through turning the backlight completely off and fading it on
+            case 0:                                                             //backlight is off
+                opt.setBrightness(0);                                           //set backlight to off
+                brightness = 1;                                                 //prepare the brightness variable for exponential increasing
+                brightState++;                                                  //advance to the next state
+                break;
+            case 1:                                                             //backlight is fading on
+                if(brightness <= 256) {                                         //if the backlight is not at max yet
+                    brightness = brightness*2;                                  //increase the current brightness by a factor of 2
+                    if(brightness >= 256) {                                     //once we get to 256
+                        opt.setBrightness(255);                                 //set the brightness to 255 instead, the max byte var size
+                        brightness = 0;                                         //reset the brightness var
+                        brightState = 0;                                        //turn the backlight off next run through
+                    }
+                    else {                                                      //if we are not at 256 yet
+                        opt.setBrightness(brightness);                          //set the current brightness value
+                    }
+                }
+                break;
+        }     
+    }
+    
+    private void toggleRainbow() {
+        
+    }
+    
+    private void prevTweet() {
+        
+    }
+    
+    private void pauseScroll() {
+        
+    }
+    
+    private void switchColor() {
+        
+    }
     
     
-    
-    //    public void fnHandler(int in) {
-//        if(in == 1) {
-//            switchBrightness();
-//        }
-//        else if(in == 2) {
-//            
-//        }
-//    }
-//    
-//    private void switchBrightness() {
-//        
-//        b++;
-//        if(b == 6) {
-//            b = 0;
-//        }
-//        switch(b) {
-//            case 0:
-//                brightness = 255;
-//                break;
-//            case 1:
-//                brightness = 128;
-//                break;
-//            case 2:
-//                brightness = 64;
-//                break;
-//            case 3:
-//                brightness = 32;
-//                break;
-//            case 4:
-//                brightness = 16;
-//                break;
-//            case 5:
-//                brightness = 0;
-//                break;
-//            default:
-//                break;
-//            }
-//    }
+
+
 }
