5ec0f3a31fb134914d346a2e7125a2458eb4f003
HybridAir <s.tratton.blake@gmail.com>
2014-05-20 10:06:26 -0700

    Organized and updated comments

--- src/twiscnhost/DeviceHandler.java
+++ src/twiscnhost/DeviceHandler.java
@@ -1,55 +1,39 @@
 package twiscnhost;
 
-import java.awt.Color;
-
 public class DeviceHandler {
     
-    private DeviceOptions options;
+    private Options options;
     private DeviceComms comms;
     
-    public DeviceHandler(int[] deviceIDs, DeviceOptions options) {
+    public DeviceHandler(int[] deviceIDs, Options options) {                    //constructor, needs the device IDs to use and the options instance
         this.options = options;
         System.out.println("Attempting to connect to device with VENDOR_ID: " + deviceIDs[0] + " and PRODUCT_ID: " + deviceIDs[1]);
         comms = new DeviceComms(new UsbHidComms(deviceIDs[0], deviceIDs[1]));   //try connecting to the device over usb, program will not continue until successful
         applyAllOptions();   
-       // options.setLCDColor(Color.GREEN);
-        //options.setBrightness(100);
-        //options.setBlinkState(true);
-        //options.setLCDColor(Color.red);
-        //options.setBlinkSpd(100);
-       // comms.sendRaw("$c" + options.getLCDColor());
-      //  comms.sendRaw("=");
-       // System.out.println("$d" + options.getBlinkState() + options.getBlinkSpd() + options.getBlinkColor());
-      //  comms.sendRaw("$d" + options.getBlinkState() + options.getBlinkSpd() + options.getBlinkColor());
-        //comms.sendRaw("=");
     }
     
-    public void applyAllOptions() {
-        comms.sendOptions(options.formatAll());
+//==============================================================================    
+    
+    public void applyAllOptions() {                                             //applys all options, use this after updating option settings
+        comms.sendOptions(options.formatAll());                                 //format and send the updated options to the device
     }
     
-    public void newTweet(String user, String text) {
+    public void newTweet(String user, String text) {                            //used to send a new tweet to the device, needs a user and tweet text
         comms.row0(user);
-//        try {
-//                        Thread.sleep(1000L);                                    //2 seconds should be enough time for the device to get ready
-//                    } catch (Exception e) {}
         comms.row1(text);
     }
          
-    public void monitorFNs() {
+    public void monitorFNs() {                                                  //used to monitor the device's fn buttons, needs to be ran continuously
         String in = comms.monitor();
         if(in != null) {
             System.out.print(in);
-            if(in.equals("FN1")) {
-                options.fnHandler(1);
+            if(in.equals("1")) {                                                
                 try {
-                        Thread.sleep(50L);                                    //2 seconds should be enough time for the device to get ready
+                        Thread.sleep(500L);                                    //2 seconds should be enough time for the device to get ready
                     } catch (Exception e) {}
-                applyAllOptions();
-            }
-            else if (in.equals("FN2")) {
-                options.fnHandler(2);
-                applyAllOptions();
+                comms.sendRaw("$g");
+                comms.sendRaw("=");
+                System.out.println("sent");
             }
         }
     }
