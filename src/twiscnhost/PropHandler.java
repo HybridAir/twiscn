46bf1f2689295fcc8139d7672d96c772f741ba64
HybridAir <s.tratton.blake@gmail.com>
2014-05-23 15:21:08 -0700

    Updated comments and added additional exception checking code

--- src/twiscnhost/PropHandler.java
+++ src/twiscnhost/PropHandler.java
@@ -3,6 +3,8 @@ package twiscnhost;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.InputStream;
 import java.util.Properties;
@@ -24,37 +26,66 @@ public class PropHandler {
     
     
     private void findFile() {                                                   //used to check if the config file exists, and create it if necessary/possible
-        byte attempts = 0;
-        checkExists();                                                          //check if the config file exists
-        while(!exists) {                                                        //while the config file doesn't exist
+        byte attempts = 0;                                                      //used to track attempts
+        while(!exists && usingConfig) {                                         //while the file doesn't exist, and we are actually going to use the file
             checkExists();                                                      //check if the file exists
-            createFile();                                                       //if we get here, the file does not exist, so try to create it
-            attempts++;                                                         //increase the attempt amount
+            attempts++;                                                         //file was not found if we got here, so increase the attempt amount
             if(attempts == 3) {                                                 //if we failed at creating the file 3 times
                 //failsafe, the program can still run without a config file
-                System.out.println("Failed to create config file 3 times. Proceeding without config file.");
-                usingConfig = false;
+                System.out.println("Failed to find/create config file 3 times. Proceeding without loading/saving settings.");
+                usingConfig = false;                                            //let the program know not to use a config file
             }
         }
     }
     
     private void checkExists() {
         try {
-            input = new FileInputStream("goop.properties");
+            input = new FileInputStream("7rep.properties");
+            System.out.println("Found configuration file.");
             exists = true;          
         }
         catch(FileNotFoundException e) {
-            System.out.println("Configuration file does not exist, creating it now.");
+            System.out.println("Configuration file was not found, attempting to create now.");
             exists = false;
-	}       
+            createFile();                                                       //the file was not found, so try to create it
+	}
+        finally {
+            if (input != null) {
+		try {
+                    input.close();
+		} 
+                catch (IOException e) {
+                    e.printStackTrace();
+		}
+            }
+        }
     }
     
     private void createFile() {
-        
+        try {
+            output = new FileOutputStream("7rep.properties");
+            System.out.println("New configuration file was created.");
+	} 
+        catch (IOException io) {
+            System.out.println("Unable to create configuration file.");
+            io.printStackTrace();
+	} 
+        finally {
+            if (output != null) {
+                try {
+                    output.close();
+		} 
+                catch (IOException e) {
+                    e.printStackTrace();
+		}
+            }
+	}    
     }
     
     private void checkIntegrity() {
-        
+        if(usingConfig) {
+            
+        }
     }
     
 }
