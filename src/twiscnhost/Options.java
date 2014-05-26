6dd23479dc8e7d45da483b1dcb9a8a8777b38679
HybridAir <s.tratton.blake@gmail.com>
2014-05-25 17:51:50 -0700

    Added second part of property integrity checking, added option setting from property values

--- src/twiscnhost/Options.java
+++ src/twiscnhost/Options.java
@@ -7,7 +7,7 @@ import java.awt.Color;
 
 public class Options {
     
-    //set default option variables   
+    //set default option variables      
     private Color lcdCol = new Color(0, 150, 255);  
     private Color blinkCol = Color.YELLOW; 
     private int rnbwSpd = 100;   
@@ -20,6 +20,16 @@ public class Options {
     private boolean scroll = true;
     private byte fn1Action = 3;
     private byte fn2Action = 0;
+    
+    //max values for certain options
+    private final int MAXBRIGHTNESS = 255;
+    private final int MAXBLINKSPD = 999;
+    private final int MAXRNBWSPD = 65535;
+    private final int MAXREADTIME = 65535;
+    private final int MAXACTION = 3;
+    
+    public String[] propValues;
+    public String[] propNames;
 
     public Options() {                                                          //default constructor, use all default settings  
     }
@@ -77,6 +87,55 @@ public class Options {
             String.valueOf(blinkSpd), getRnbwState(), String.valueOf(rnbwSpd), 
             String.valueOf(readTime), getFn1Str(), getFn2Str()};
         return out;
+    }
+    
+    public void setPropValue(int valueID, int in) {                             //used to set the options here to values read from the config file, fixing them is necessary
+        //needs the property value ID and the value
+        switch(valueID) {
+            case 0:
+                setBrightness(checkValLimits(MAXBRIGHTNESS, in));
+                break;
+            case 1:
+                setLCDColor(new Color(in));
+                break;
+            case 2:
+                setBlinkState((checkValLimits(1, in)) <= 0 ? true : false);     //convert the int to a boolean
+                break;
+            case 3:
+                setBlinkColor(new Color(in));
+                break;
+            case 4:
+                setBlinkSpd(checkValLimits(MAXBLINKSPD, in));
+                break;
+            case 5:
+                setRnbwState((checkValLimits(1, in)) <= 0 ? true : false);      //convert the int to a boolean
+                break;
+            case 6:
+                setRnbwSpd(checkValLimits(MAXRNBWSPD, in));
+                break;
+            case 7:
+                setReadTime(checkValLimits(MAXREADTIME, in));
+                break;
+            case 8:
+                setFn1Action((byte)checkValLimits(MAXACTION, in));
+                break;
+            case 9:
+                setFn2Action((byte)checkValLimits(MAXACTION, in));
+                break;
+        }
+    }
+    
+    private int checkValLimits(int max, int in) {                               //used to ensure the property values are not outside their limits
+        if(in > max) {                                                          //if the value is greater than the max it can be
+            System.out.println("got " + in);
+            return max;                                                         //return the max value
+        }
+        else if(in < 0) {                                                       //vice versa, all option values are never less than 0
+            System.out.println("got " + in);
+            return 0;
+        }
+        else                                                                    //property value should be good, just return it as is
+            return in;    
     }
     
 //==============================================================================  
