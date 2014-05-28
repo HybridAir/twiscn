90d0facbafb08aca885ffad6bdbabce757a39f88
HybridAir <s.tratton.blake@gmail.com>
2014-05-27 17:35:34 -0700

    Added latest tweet processing code and additional exception handling

--- src/twiscnhost/TweetHandler.java
+++ src/twiscnhost/TweetHandler.java
@@ -1,43 +1,97 @@
 //used to handle most twitter related functionality
 package twiscnhost;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import twitter4j.*;
 
 public class TweetHandler {
     
-    private long[] following;
+    Options opt;
+    DeviceHandler twiScn;
+    TwitterStream twitterStream;
+    Twitter twitter;
    
-    public TweetHandler() {
-        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+    public TweetHandler(Options opt, DeviceHandler twiScn) {
+        this.opt = opt;
+        this.twiScn = twiScn;
+        twitter = new TwitterFactory().getInstance();
+        twitterStream = new TwitterStreamFactory().getInstance();
         twitterStream.addListener(listener);
         
-        
-        long[] followJerp = {18856582};
-        
-        twitterStream.filter(new FilterQuery(followJerp));
-        
-        System.out.println("TwitterStream started");
-
+        sendLatestTweet();
     }
     
-    public void startStream() {
+    public String[] getLatestTweet() {
+        try {
+            Query query = new Query(getUserQuery());
+            QueryResult result = twitter.search(query);
+            List<Status> statuses = result.getTweets();
+            Status status = statuses.get(0);
+            return formatTweet(status);
+        } catch (TwitterException te) {
+            //te.printStackTrace();
+            System.out.println("Failed to get latest tweet: " + te.getMessage());
+            return null;
+        }
+    }
     
+    private String[] formatTweet(Status status) {
+            Date createdAt = status.getCreatedAt();
+            SimpleDateFormat ft = new SimpleDateFormat (" hh:mm a");
+            String user = status.getUser().getScreenName() + ":";
+            String text = status.getText() + " " + ft.format(createdAt);
+            String[] out = {user, text};
+            return out;
+    }
+    
+    public void sendLatestTweet() {
+        String[] tweet = getLatestTweet();
+        if(tweet != null) {
+            twiScn.newTweet(tweet[0], tweet[1]);
+        }
+    }
+  
+    public long getUserID(String screenName) throws TwitterException {
+        //try {
+            User user = twitter.showUser(screenName);
+            return user.getId();
+        //} 
+        //catch (TwitterException te) {
+        //    te.printStackTrace();
+        //    return 0;
+        //}
+    }
+    
+    public String getScreenName(long ID) throws TwitterException {
+        User user = twitter.showUser(ID);
+        return user.getScreenName();
+    }
+    
+    private String getUserQuery() throws TwitterException {      
+        long[] users = opt.getFollowUsers(); 
+        String out = "";
+        for(int i = 0;i <= users.length - 1;i++) { 
+            out += "from:" + getScreenName(users[i]);
+            if(i < users.length - 1) {                                             //if we are not on the last ID
+                out += " OR ";                                                     //add a , to be used as a delimiter
+            }
+        }
+        return out;
+    }
+    
+//==============================================================================
+    
+    public void startStream() {
+        twitterStream.filter(new FilterQuery(opt.getFollowUsers()));    
+        System.out.println("TwitterStream started");
     }
     
     public void stopStream() {
-        
+        twitterStream.shutdown();
+        System.out.println("TwitterStream stopped");
     }
-    
-    public void getFollowers() {
-        
-    }
-    
-    public void getUserID() {
-        
-    }
-    
-    
-    
     
      private static final UserStreamListener listener = new UserStreamListener() {
         @Override
