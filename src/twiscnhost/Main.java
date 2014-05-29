c581034bab59c1ad421bf5b103bc13b04a8381a1
HybridAir <s.tratton.blake@gmail.com>
2014-05-29 12:44:38 -0700

    Implemented logging to file

--- src/twiscnhost/Main.java
+++ src/twiscnhost/Main.java
@@ -1,16 +1,20 @@
 //Main console-based host program for controlling the TwiScnDevice
-//TODO: detect handshake while running?, gui (tray application), error/general logging, handle protected users
-//test
+//TODO: detect handshake while running?, gui (tray application), handle protected users, add additional exception checking to hidcomms
 package twiscnhost;
 
-public class Main {
+import java.util.logging.*;
 
+public class Main {
+    
+    private final static Logger logger = Logger.getLogger(LogHandler.class.getName());
     public static final int VENDOR_ID = 0x16c0;                         
     public static final int PRODUCT_ID = 0x27d9;
     public static final int[] DEVICEIDS = {VENDOR_ID, PRODUCT_ID};
     public static Options devOptions;
 
     public static void main(String[] args) {
+        LogHandler.init();
+        
         devOptions = new Options();                                             //create a new instance of DeviceOptions     
         PropHandler props = new PropHandler(devOptions);        
         DeviceHandler twiScn = new DeviceHandler(DEVICEIDS, devOptions);        //create a new instance of DeviceHandler, needs the device ids and options 
