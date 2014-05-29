62cf740a07952f280d0586ffcd20bcf75cb3441b
HybridAir <s.tratton.blake@gmail.com>
2014-05-29 12:44:25 -0700

    Implemented logging to file

--- src/twiscnhost/TweetHandler.java
+++ src/twiscnhost/TweetHandler.java
@@ -4,10 +4,12 @@ package twiscnhost;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
 import twitter4j.*;
 
 public class TweetHandler {
     
+    private final static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(LogHandler.class.getName());
     Options opt;
     DeviceHandler twiScn;
     TwitterStream twitterStream;
@@ -47,9 +49,9 @@ public class TweetHandler {
             twiScn.newTweet(tweet[0], tweet[1]);                                //send the tweet to the device
         }
         catch (TwitterException te) {                                           //catch any TwitterException that could have happened here
-            System.err.println("Failed to send latest tweet:");
-            te.printStackTrace();
-            //cant send anything new to the device
+            logger.log(Level.WARNING, "Failed to send Latest Tweet", te);
+            //cant send anything new to the device, so pretend this never happened
+            //might want to kill the program with an error actually, could be a twitter problem
         }
     }
     
@@ -87,168 +89,167 @@ public class TweetHandler {
     
     public void startStream() {                                                 //starts the twitterStream thread
         twitterStream.filter(new FilterQuery(opt.getFollowUsers()));    
-        System.out.println("TwitterStream started");
+        logger.log(Level.INFO, "TwitterStream started");
     }
     
     public void stopStream() {                                                  //stops the twitterStream thread
         twitterStream.shutdown();
-        System.out.println("TwitterStream stopped");
+        logger.log(Level.INFO, "TwitterStream stopped");
     }
     
     private final UserStreamListener listener = new UserStreamListener() {      //UserStreamListener implementation
         @Override
         public void onStatus(Status status) {
-            System.out.println("onStatus @" + status.getUser().getScreenName() + " - " + status.getText());
+            //System.out.println("onStatus @" + status.getUser().getScreenName() + " - " + status.getText());
             sendTweet(status);
         }
 
         @Override
         public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-            System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+            //System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
             sendLatestTweet();
         }
 
         @Override
         public void onDeletionNotice(long directMessageId, long userId) {
-            System.out.println("Got a direct message deletion notice id:" + directMessageId);
+            //System.out.println("Got a direct message deletion notice id:" + directMessageId);
         }
 
         @Override
         public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-            System.out.println("Got a track limitation notice:" + numberOfLimitedStatuses);
+            //System.out.println("Got a track limitation notice:" + numberOfLimitedStatuses);
         }
 
         @Override
         public void onScrubGeo(long userId, long upToStatusId) {
-            System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            //System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
         }
 
         @Override
         public void onStallWarning(StallWarning warning) {
-            System.out.println("Got stall warning:" + warning);
+            //System.out.println("Got stall warning:" + warning);
         }
 
         @Override
         public void onFriendList(long[] friendIds) {
-            System.out.print("onFriendList");
-            for (long friendId : friendIds) {
-                System.out.print(" " + friendId);
-            }
-            System.out.println();
+            //System.out.print("onFriendList");
+            //for (long friendId : friendIds) {
+               // System.out.print(" " + friendId);
+            //}
+            //System.out.println();
         }
 
         @Override
         public void onFavorite(User source, User target, Status favoritedStatus) {
-            System.out.println("onFavorite source:@"
-                    + source.getScreenName() + " target:@"
-                    + target.getScreenName() + " @"
-                    + favoritedStatus.getUser().getScreenName() + " - "
-                    + favoritedStatus.getText());
+            //System.out.println("onFavorite source:@"
+            //        + source.getScreenName() + " target:@"
+            //        + target.getScreenName() + " @"
+            //        + favoritedStatus.getUser().getScreenName() + " - "
+            //        + favoritedStatus.getText());
         }
 
         @Override
         public void onUnfavorite(User source, User target, Status unfavoritedStatus) {
-            System.out.println("onUnFavorite source:@"
-                    + source.getScreenName() + " target:@"
-                    + target.getScreenName() + " @"
-                    + unfavoritedStatus.getUser().getScreenName()
-                    + " - " + unfavoritedStatus.getText());
+            //System.out.println("onUnFavorite source:@"
+            //        + source.getScreenName() + " target:@"
+            //        + target.getScreenName() + " @"
+            //        + unfavoritedStatus.getUser().getScreenName()
+            //        + " - " + unfavoritedStatus.getText());
         }
 
         @Override
         public void onFollow(User source, User followedUser) {
-            System.out.println("onFollow source:@"
-                    + source.getScreenName() + " target:@"
-                    + followedUser.getScreenName());
+            //System.out.println("onFollow source:@"
+            //        + source.getScreenName() + " target:@"
+            //        + followedUser.getScreenName());
         }
 
         @Override
         public void onUnfollow(User source, User followedUser) {
-            System.out.println("onFollow source:@"
-                    + source.getScreenName() + " target:@"
-                    + followedUser.getScreenName());
+            //System.out.println("onFollow source:@"
+            //        + source.getScreenName() + " target:@"
+            //        + followedUser.getScreenName());
         }
 
         @Override
         public void onDirectMessage(DirectMessage directMessage) {
-            System.out.println("onDirectMessage text:"
-                    + directMessage.getText());
+            //System.out.println("onDirectMessage text:"
+            //        + directMessage.getText());
         }
 
         @Override
         public void onUserListMemberAddition(User addedMember, User listOwner, UserList list) {
-            System.out.println("onUserListMemberAddition added member:@"
-                    + addedMember.getScreenName()
-                    + " listOwner:@" + listOwner.getScreenName()
-                    + " list:" + list.getName());
+            //System.out.println("onUserListMemberAddition added member:@"
+            //        + addedMember.getScreenName()
+            //        + " listOwner:@" + listOwner.getScreenName()
+            //        + " list:" + list.getName());
         }
 
         @Override
         public void onUserListMemberDeletion(User deletedMember, User listOwner, UserList list) {
-            System.out.println("onUserListMemberDeleted deleted member:@"
-                    + deletedMember.getScreenName()
-                    + " listOwner:@" + listOwner.getScreenName()
-                    + " list:" + list.getName());
+            //System.out.println("onUserListMemberDeleted deleted member:@"
+            //        + deletedMember.getScreenName()
+            //       + " listOwner:@" + listOwner.getScreenName()
+            //        + " list:" + list.getName());
         }
 
         @Override
         public void onUserListSubscription(User subscriber, User listOwner, UserList list) {
-            System.out.println("onUserListSubscribed subscriber:@"
-                    + subscriber.getScreenName()
-                    + " listOwner:@" + listOwner.getScreenName()
-                    + " list:" + list.getName());
+            //System.out.println("onUserListSubscribed subscriber:@"
+            //        + subscriber.getScreenName()
+            //        + " listOwner:@" + listOwner.getScreenName()
+            //        + " list:" + list.getName());
         }
 
         @Override
         public void onUserListUnsubscription(User subscriber, User listOwner, UserList list) {
-            System.out.println("onUserListUnsubscribed subscriber:@"
-                    + subscriber.getScreenName()
-                    + " listOwner:@" + listOwner.getScreenName()
-                    + " list:" + list.getName());
+            //System.out.println("onUserListUnsubscribed subscriber:@"
+            //        + subscriber.getScreenName()
+            //        + " listOwner:@" + listOwner.getScreenName()
+            //        + " list:" + list.getName());
         }
 
         @Override
         public void onUserListCreation(User listOwner, UserList list) {
-            System.out.println("onUserListCreated  listOwner:@"
-                    + listOwner.getScreenName()
-                    + " list:" + list.getName());
+            //System.out.println("onUserListCreated  listOwner:@"
+            //        + listOwner.getScreenName()
+            //        + " list:" + list.getName());
         }
 
         @Override
         public void onUserListUpdate(User listOwner, UserList list) {
-            System.out.println("onUserListUpdated  listOwner:@"
-                    + listOwner.getScreenName()
-                    + " list:" + list.getName());
+            //System.out.println("onUserListUpdated  listOwner:@"
+            //        + listOwner.getScreenName()
+            //        + " list:" + list.getName());
         }
 
         @Override
         public void onUserListDeletion(User listOwner, UserList list) {
-            System.out.println("onUserListDestroyed  listOwner:@"
-                    + listOwner.getScreenName()
-                    + " list:" + list.getName());
+            //System.out.println("onUserListDestroyed  listOwner:@"
+            //        + listOwner.getScreenName()
+            //        + " list:" + list.getName());
         }
 
         @Override
         public void onUserProfileUpdate(User updatedUser) {
-            System.out.println("onUserProfileUpdated user:@" + updatedUser.getScreenName());
+            //System.out.println("onUserProfileUpdated user:@" + updatedUser.getScreenName());
         }
 
         @Override
         public void onBlock(User source, User blockedUser) {
-            System.out.println("onBlock source:@" + source.getScreenName()
-                    + " target:@" + blockedUser.getScreenName());
+            //System.out.println("onBlock source:@" + source.getScreenName()
+            //        + " target:@" + blockedUser.getScreenName());
         }
 
         @Override
         public void onUnblock(User source, User unblockedUser) {
-            System.out.println("onUnblock source:@" + source.getScreenName()
-                    + " target:@" + unblockedUser.getScreenName());
+            //System.out.println("onUnblock source:@" + source.getScreenName()
+            //        + " target:@" + unblockedUser.getScreenName());
         }
 
         @Override
         public void onException(Exception ex) {
-            ex.printStackTrace();
-            System.out.println("onException:" + ex.getMessage());
+            logger.log(Level.WARNING, "TwitterStream got an exception", ex);
         }
     };     
 }
