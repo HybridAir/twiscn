ab9106502c2fd7022cc537fa54059957d50d8560
HybridAir <s.tratton.blake@gmail.com>
2014-06-02 12:23:44 -0700

    Moved code to fix dependency problems

--- src/twiscnhost/Main.java
+++ src/twiscnhost/Main.java
@@ -1,5 +1,5 @@
 //Main console-based host program for controlling the TwiScnDevice
-//TODO: gui (tray application), check twitter connection first
+//TODO: gui (tray application), check twitter connection first, device sleep button
 package twiscnhost;
 
 import java.util.logging.*;
@@ -10,23 +10,32 @@ public class Main extends javax.swing.JFrame {
     public static final int VENDOR_ID = 0x16c0;                         
     public static final int PRODUCT_ID = 0x27d9;
     public static final int[] DEVICEIDS = {VENDOR_ID, PRODUCT_ID};
-    public static Options devOptions;
+    
+    public static Options devOptions = new Options();
+    public static PropHandler props = new PropHandler(devOptions);        
+    public static Gui gui = new Gui(devOptions);
+    public static DeviceHandler twiScn = new DeviceHandler(DEVICEIDS, devOptions, gui);   //create a new instance of DeviceHandler, needs the device ids and options 
+    public static TweetHandler twt = new TweetHandler(devOptions, twiScn);
 
     public static void main(String[] args) {
-        LogHandler.init();
-        Gui goopy = new Gui();
-        devOptions = new Options();                                             //create a new instance of DeviceOptions     
-        PropHandler props = new PropHandler(devOptions);        
-        DeviceHandler twiScn = new DeviceHandler(DEVICEIDS, devOptions);        //create a new instance of DeviceHandler, needs the device ids and options 
-        TweetHandler twt = new TweetHandler(devOptions, twiScn);
+        LogHandler.init();                                                      //start logging
+        gui.setTwt(twt);
+        twiScn.init();
+        twt.init();
         
         while(true) {
             while(UsbHidComms.connected()) {                                    //keep doing this stuff while the device is still connected
                 //check for button updates
+                if(gui.applyOptions) {
+                    twiScn.applyAllOptions();
+                    gui.applyOptions = false;
+                }
                 twiScn.monitorFNs();
             }
+                
             
             //attempt reconnecting here
+            gui.setConnected(false);
             logger.log(Level.WARNING, "Lost connection to device.");
             twt.stopStream();
             twiScn.init();
