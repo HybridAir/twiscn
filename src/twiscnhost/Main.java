8e2440947ed5b80ac4f809e029b48e20b6f9d759
HybridAir <s.tratton.blake@gmail.com>
2014-06-05 14:18:01 -0700

    Implemented device sleeping/disconnecting in general, and on program shutdown

--- src/twiscnhost/Main.java
+++ src/twiscnhost/Main.java
@@ -3,6 +3,8 @@
 package twiscnhost;
 
 import java.util.logging.*;
+import static twiscnhost.Main.opt;
+import static twiscnhost.Main.twiScn;
 
 public class Main extends javax.swing.JFrame {
     
@@ -17,30 +19,49 @@ public class Main extends javax.swing.JFrame {
     public static final Gui gui = new Gui(opt);
     public static final DeviceHandler twiScn = new DeviceHandler(DEVICEIDS, opt, gui);   //create a new instance of DeviceHandler, needs the device ids and options 
     public static final TweetHandler twt = new TweetHandler(opt, twiScn);
+    public static final ShutdownHook shutdown = new ShutdownHook();
 
     public static void main(String[] args) {
+        shutdown.attachShutDownHook();
         opt.init(props);
         gui.init(twt);
         twiScn.init();
         twt.init();
         
         while(true) {
-            while(UsbHidComms.connected()) {                                    //keep doing this stuff while the device is still connected
-                //check for button updates
-                if(gui.applyTwitter) {
+            if(UsbHidComms.connected()) {                                       //keep doing this stuff while the device is still connected
+                if(gui.applyDevice) {                                           //check if device settings need to be applied to the device, and saved
+                    props.writeAllProps(false);
+                    twiScn.applyAllOptions();
+                    gui.applyDevice = false;
+                }
+                if(gui.applyTwitter) {                                          //check if twitter settings need to be saved
                     props.writeAllProps(false);
                     gui.applyTwitter = false;
                 }
-                twiScn.monitorFNs();
+                twiScn.monitorFNs();                                            //monitor the FN buttons
             }
-                
-            
-            //attempt reconnecting here
-            gui.setConnected(false);
-            logger.log(Level.WARNING, "Lost connection to device.");
-            twt.stopStream();
-            twiScn.init();
-            twt.init();
+            else {                                                              //device got disconnected, try reconnecting it
+                gui.setConnected(false);
+                logger.log(Level.WARNING, "Lost connection to device.");
+                twt.stopStream();
+                twiScn.init();
+                twt.sendLatestTweet();
+            }
         }
+    }
+}
+
+class ShutdownHook {                                                                         
+    public void attachShutDownHook() {                                          //used to monitor when the program is told to shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                if(UsbHidComms.connected()) {                                   //make sure the device is connected first
+                    opt.setSleep(true);                                         //set sleep to true
+                    twiScn.applyAllOptions();                                   //tell the device to sleep/disconnect
+                }
+            }
+        });
     }
 }
