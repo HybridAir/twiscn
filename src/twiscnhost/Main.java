0f24f82bf29d0166f3273b508d962f4982a0fb87
HybridAir <s.tratton.blake@gmail.com>
2014-06-05 17:13:34 -0700

    Implemented keepalive code

--- src/twiscnhost/Main.java
+++ src/twiscnhost/Main.java
@@ -1,5 +1,5 @@
 //Main console-based host program for controlling the TwiScnDevice
-//TODO: device sleep button
+//TODO: device sleep button, keep alive signal maybe, killing the trayicon on exit
 package twiscnhost;
 
 import java.util.logging.*;
@@ -8,7 +8,7 @@ import static twiscnhost.Main.twiScn;
 
 public class Main extends javax.swing.JFrame {
     
-    private final static Logger logger = Logger.getLogger(LogHandler.class.getName());
+    private static final Logger logger = Logger.getLogger(LogHandler.class.getName());
     public static final int VENDOR_ID = 0x16c0;                         
     public static final int PRODUCT_ID = 0x27d9;
     public static final int[] DEVICEIDS = {VENDOR_ID, PRODUCT_ID};
@@ -17,8 +17,9 @@ public class Main extends javax.swing.JFrame {
     public static final Options opt = new Options();
     public static final PropHandler props = new PropHandler(opt);        
     public static final Gui gui = new Gui(opt);
-    public static final DeviceHandler twiScn = new DeviceHandler(DEVICEIDS, opt, gui);   //create a new instance of DeviceHandler, needs the device ids and options 
+    public static final DeviceHandler twiScn = new DeviceHandler(DEVICEIDS, opt, gui); 
     public static final TweetHandler twt = new TweetHandler(opt, twiScn);
+    public static final KeepAlive keepAlive = new KeepAlive();
     public static final ShutdownHook shutdown = new ShutdownHook();
 
     public static void main(String[] args) {
@@ -26,11 +27,12 @@ public class Main extends javax.swing.JFrame {
         opt.init(props);
         gui.init(twt);
         twiScn.init();
+        keepAlive.start();
         twt.init();
         
         while(true) {
             if(UsbHidComms.connected()) {                                       //keep doing this stuff while the device is still connected
-                if(gui.applyDevice) {                                           //check if device settings need to be applied to the device, and saved
+                if(gui.applyDevice) {                                           //check if device settings need to be applied to the device and saved
                     props.writeAllProps(false);
                     twiScn.applyAllOptions();
                     gui.applyDevice = false;
@@ -39,7 +41,7 @@ public class Main extends javax.swing.JFrame {
                     props.writeAllProps(false);
                     gui.applyTwitter = false;
                 }
-                twiScn.monitorFNs();                                            //monitor the FN buttons
+                twiScn.monitorFNs();                                            //monitor the FN buttons for updates
             }
             else {                                                              //device got disconnected, try reconnecting it
                 gui.setConnected(false);
@@ -58,10 +60,23 @@ class ShutdownHook {
             @Override
             public void run() {
                 if(UsbHidComms.connected()) {                                   //make sure the device is connected first
-                    opt.setSleep(true);                                         //set sleep to true
-                    twiScn.applyAllOptions();                                   //tell the device to sleep/disconnect
+                    //opt.setSleep(true);                                         //set sleep to true
+                    //twiScn.applyAllOptions();                                   //tell the device to sleep/disconnect
                 }
             }
         });
     }
 }
+
+class KeepAlive extends Thread {                                                //thread used to send keep alive packets to the device every 100 ms
+     public void run() {
+         while(true) {
+            if(UsbHidComms.connected()) {                                       //make sure the device is connected before sending it keepalives
+                twiScn.keepAlive();                                             //send a keep alive packet
+                try {                                                           //try to wait 100 ms before sending another
+                    Thread.sleep(100L);
+                } catch (Exception e) {}
+            }
+         }
+     }
+ }
