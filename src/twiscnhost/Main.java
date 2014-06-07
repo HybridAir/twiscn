d769bde6ab31b05fb16eaacc4911dea89ecdfea3
HybridAir <s.tratton.blake@gmail.com>
2014-06-07 09:15:40 -0700

    Moved the main operation to its own thread to ensure the device handshake gets caught on time

--- src/twiscnhost/Main.java
+++ src/twiscnhost/Main.java
@@ -1,11 +1,13 @@
 //Main console-based host program for controlling the TwiScnDevice
-//TODO: 
+//TODO: move handshake check to the cxheckalive thread
 package twiscnhost;
 
 import java.util.logging.*;
 import static twiscnhost.Main.gui;
 import static twiscnhost.Main.opt;
+import static twiscnhost.Main.props;
 import static twiscnhost.Main.twiScn;
+import static twiscnhost.Main.twt;
 
 public class Main extends javax.swing.JFrame {
     
@@ -21,6 +23,7 @@ public class Main extends javax.swing.JFrame {
     public static final DeviceHandler twiScn = new DeviceHandler(DEVICEIDS, opt, gui); 
     public static final TweetHandler twt = new TweetHandler(opt, twiScn);
     public static final KeepAlive keepAlive = new KeepAlive();
+    public static final DeviceMon devMon = new DeviceMon();
     public static final ShutdownHook shutdown = new ShutdownHook();
 
     public static void main(String[] args) {
@@ -30,8 +33,68 @@ public class Main extends javax.swing.JFrame {
         twiScn.init();
         keepAlive.start();
         twt.init();
+        devMon.start();
         
-        while(true) {
+//        while(true) {
+//            if(UsbHidComms.connected() && DeviceComms.connected) {              //keep doing this stuff while the device is still connected
+//                if(gui.applyDevice) {                                           //check if device settings need to be applied to the device and saved
+//                    props.writeAllProps(false);
+//                    twiScn.applyAllOptions();
+//                    gui.applyDevice = false;
+//                }
+//                if(gui.applyTwitter) {                                          //check if twitter settings need to be saved
+//                    props.writeAllProps(false);
+//                    gui.applyTwitter = false;
+//                }
+//                twiScn.monitorDevice();                                         //monitor the FN buttons for updates
+//            }
+//            else {
+//                //device got disconnected, try reconnecting it
+//                gui.setConnected(false);
+//                gui.addStatusLine("Lost connection to device.");
+//                logger.log(Level.WARNING, "Lost connection to device.");
+//                twt.stopStream();
+//                twiScn.init();
+//                gui.setDeviceDefaults();
+//                gui.applyDevice = true;
+//                gui.applyTwitter = true;
+//                twt.sendLatestTweet();
+//                twt.startStream();
+//            }
+//        }
+    }
+}
+
+class ShutdownHook {                                                                         
+    public void attachShutDownHook() {                                          //used to monitor when the program is told to shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                Logger logger = Logger.getLogger(LogHandler.class.getName());
+                logger.log(Level.WARNING, "Program exiting.");
+            }
+        });
+    }
+}
+
+class KeepAlive extends Thread {                                                //thread used to send keep alive packets to the device every 100 ms
+     public void run() {
+         while(true) {
+            if(UsbHidComms.connected() && DeviceComms.connected) {              //make sure the device is connected before sending it keep alives
+                twiScn.keepAlive();                                             //send a keep alive packet      
+            }
+            try {                                                               //try to wait 100 ms before sending another
+                Thread.sleep(750L);
+            } catch (Exception e) {}
+         }
+     }
+ }
+
+class DeviceMon extends Thread {                                                //thread used to send keep alive packets to the device every 100 ms
+     public void run() {
+         //while(true) {
+             Logger logger = Logger.getLogger(LogHandler.class.getName());
+                    while(true) {
             if(UsbHidComms.connected() && DeviceComms.connected) {              //keep doing this stuff while the device is still connected
                 if(gui.applyDevice) {                                           //check if device settings need to be applied to the device and saved
                     props.writeAllProps(false);
@@ -58,30 +121,6 @@ public class Main extends javax.swing.JFrame {
                 twt.startStream();
             }
         }
-    }
-}
-
-class ShutdownHook {                                                                         
-    public void attachShutDownHook() {                                          //used to monitor when the program is told to shutdown
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                Logger logger = Logger.getLogger(LogHandler.class.getName());
-                logger.log(Level.WARNING, "Program exiting.");
-            }
-        });
-    }
-}
-
-class KeepAlive extends Thread {                                                //thread used to send keep alive packets to the device every 100 ms
-     public void run() {
-         while(true) {
-            if(UsbHidComms.connected() && DeviceComms.connected) {              //make sure the device is connected before sending it keep alives
-                twiScn.keepAlive();                                             //send a keep alive packet      
-            }
-            try {                                                               //try to wait 100 ms before sending another
-                Thread.sleep(500L);
-            } catch (Exception e) {}
-         }
+         //}
      }
  }
